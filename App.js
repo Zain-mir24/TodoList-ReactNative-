569cde8212389edb20b7a1ed93687295c08a3182
Zain Mir <zainnaeemk10@gmail.com>
2021-04-20 01:11:26 +0500

    Styling

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text,Button, TextInput, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, Button, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <TextInput></TextInput>
-      <Button />
+      <View style={styles.inputcontainer}>
+        <TextInput style={styles.textinput}></TextInput>
+        <Button title="add" />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +18,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputcontainer: {
+    flexDirection: "row",
+    width:'70%',
+    justifyContent: "space-between",
+  },
+  textinput: {
+    borderColor: "orange",
+    borderWidth: 2,
+    width: "70%",
   },
 });

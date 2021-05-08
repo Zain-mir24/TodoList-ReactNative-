a11fba8bf7d8b75dac745d71843aaa32965bea33
Zain Mir <zainnaeemk10@gmail.com>
2021-05-09 02:16:34 +0500

    functionality

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
 import { StatusBar } from "expo-status-bar";
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, Button, TextInput, View } from "react-native";
 
 export default function App() {
-  const[getinputtext,setinputtext] = useState("DummyText")
+  const [getinputtext, setinputtext] = useState("DummyText");
   return (
     <View style={styles.container}>
       <Text style={styles.title}> My todo app</Text>
       <View style={styles.inputcontainer}>
-        <TextInput  style={styles.textinput}
-        placeholder=" Enter Item"></TextInput>
-        <Button title="add" />
+        <TextInput
+          style={styles.textinput}
+          placeholder=" Enter Item"
+          onChangeText={text=>setinputtext(text)}
+        ></TextInput>
+        <Button
+        onPress={()=>{
+          
+        }}
+         title="add" />
       </View>
       <View>
-        <Text>{getinputtext}</Text>
+        <Text style={{ fontSize: 26 }}>{getinputtext}</Text>
       </View>
 
       <StatusBar style="auto" />
@@ -22,29 +29,28 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  title:{
-    fontSize:50,
-    backgroundColor:"yellow",
-    paddingBottom:10
+  title: {
+    fontSize: 50,
+    backgroundColor: "yellow",
+    paddingBottom: 10,
   },
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
-    paddingTop:40
+    paddingTop: 40,
   },
   inputcontainer: {
     flexDirection: "row",
-    width:'70%',
+    width: "70%",
     justifyContent: "space-between",
   },
   textinput: {
     borderColor: "orange",
     borderBottomWidth: 2,
     width: "70%",
-    
-    fontSize:16,
-    padding:10,
-    
+
+    fontSize: 16,
+    padding: 10,
   },
 });
